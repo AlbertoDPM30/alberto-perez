2d707d56e54c772971d76dba8fa15f2fdb7a6f5c
alberto <albertodperez2000@gmail.com>
2025-06-27 09:15:54 -0400

    fix: position conflict with header

--- src/components/ui/CustomToolsIcons.jsx
+++ src/components/ui/CustomToolsIcons.jsx
@@ -5,16 +5,13 @@ import CustomIcons from "./CustomIcons";
 
 export default function CustomToolsIcons({ src, alt, text }) {
   const [active, setActive] = useState(false);
+
   return (
     <div
-      onMouseOver={() => {
-        setActive(true);
-      }}
-      onMouseLeave={() => {
-        setActive(false);
-      }}
-      className={`relative h-[3.4rem] flex justify-center items-center gap-1 w-full transition-all ease-out duration-1000 hover:bg-indigo-900 rounded-lg ${
-        active == true ? "shadow-md shadow-gray-700/35" : ""
+      onMouseOver={() => setActive(true)}
+      onMouseLeave={() => setActive(false)}
+      className={`relative h-[3.4rem] flex justify-center items-center gap-1 w-full transition-all ease-linear duration-700 hover:bg-indigo-900 rounded-lg ${
+        active ? "shadow-md shadow-gray-700/35" : ""
       }`}
       target="_blank"
       href="https://www.linkedin.com/in/alberto-perez-adpm"
@@ -24,13 +21,13 @@ export default function CustomToolsIcons({ src, alt, text }) {
         width={32}
         height={32}
         alt={alt}
-        styles={`absolute top-0 z-10 transition ease-in-out duration-700 ${
-          active == true ? "-translate-x-2/3" : ""
+        styles={`absolute top-0 z-0 transition ease-in-out duration-700 ${
+          active ? "-translate-x-2/3" : ""
         }`}
       />
       <span
-        className={`absolute top-0 right-0 h-[3.4rem] pr-4 flex items-center text-sm text-white font-semibold transition-all ease-out duration-400 ${
-          active == false ? "hidden" : "block"
+        className={`absolute z-0 top-0 right-0 h-[3.4rem] pr-4 flex items-center text-sm text-white font-semibold transition-all ease-in-out duration-400 ${
+          active ? "block" : "hidden"
         }`}
       >
         {text}
